e3376a203c6fb0ecb8d21c5ff93a5259f8440f4d
Preeti Karki <138830834+preetiikrak@users.noreply.github.com>
2025-06-20 01:42:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,4 +142,4 @@ def result():
                            score=score, 
                            topic_scores=topic_scores)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
